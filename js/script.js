e9398485b6bede2606c5cb8b5b994c11a72498b0
marencka <amdanz@ad.unc.edu>
2021-07-26 12:18:57 -0400

    button is fucked up. time integrated but needs fixing with width and height. changed to woff font of 95 pixelated

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+setInterval(function clock() {
+
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    let strTime = hours + ':' + minutes + ' ' + ampm;
+
+    document.getElementById("time").innerHTML = strTime;
+
+}, 1000); 
+
+
+
