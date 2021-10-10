e8002f9a7d010d5a19460d476bb9cefe6a2903ea
marencka <57925696+marencka@users.noreply.github.com>
2021-10-10 10:59:49 -0400

    Changed resume. Fixed some spacing issues. Deployed on Netlify. Need to fix start button and analytics.

--- js/script.js
+++ js/script.js
@@ -15,55 +15,6 @@ setInterval(function clock() {
 }, 100);
 
 
-
-dragElement(document.getElementById("intro"));
-dragElement(document.getElementById('aboutme-content'));
-dragElement(document.getElementById('contact-content'));
-dragElement(document.getElementById('credits-content'));
-
-
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "-header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
 function showIntro() {
   var x = document.getElementById('intro');
   if (x.style.display === 'none') {
@@ -118,14 +69,6 @@ function closeOutTerminal() {
   document.getElementById('terminal-content').style.display = 'none';
 }
 
-
-
-document.addEventListener('mouseup', function (e) {
-  var container = document.getElementById('startmenu');
-  if (!container.contains(e.target)) {
-    container.style.display = 'none';
-  }
-});
 
 function showStart() {
   var x = document.getElementById('startmenu');
